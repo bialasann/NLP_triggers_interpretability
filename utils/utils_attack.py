783605b894aac7ca603660c1332bf7e9f70df878
bialasann <89609993+bialasann@users.noreply.github.com>
2023-01-22 09:12:13 +0100

    Ban indices

--- utils/utils_attack.py
+++ utils/utils_attack.py
@@ -1,4 +1,9 @@
+import utils_model 
+
 import torch
+import heapq
+from copy import deepcopy
+from operator import itemgetter
 
 """
 Implements the HotFlip paper, which changes one token to another by using directional derivatives
@@ -6,7 +11,8 @@ of the model wrt the input.
 """
 
 def hotflip_attack(averaged_grad, embedding_matrix, trigger_token_ids,
-                   increase_loss=False, num_candidates=1):
+                   increase_loss=False, num_candidates=1,
+                   banned_idxs = None):
     """
     The "Hotflip" attack described in Equation (2) of the paper. This code is heavily inspired by
     the nice code of Paul Michel here https://github.com/pmichel31415/translate/blob/paul/
@@ -25,7 +31,9 @@ def hotflip_attack(averaged_grad, embedding_matrix, trigger_token_ids,
     # embedding_matrix.shape = [|V|, hidden_size]
     # gradient_dot_embedding_matrix -> [1, n_triggers, |V|]
     gradient_dot_embedding_matrix = torch.einsum("bij,kj->bik",
-                                                 (averaged_grad, embedding_matrix))        
+                                                 (averaged_grad, embedding_matrix)) 
+    if banned_idxs is not None:
+        gradient_dot_embedding_matrix[:, :, banned_idxs] = 100000           
     if not increase_loss:
         gradient_dot_embedding_matrix *= -1    # lower versus increase the class probability.
     if num_candidates > 1: # get top k options
@@ -35,3 +43,48 @@ def hotflip_attack(averaged_grad, embedding_matrix, trigger_token_ids,
         return best_k_ids.detach().cpu().numpy()[0]
     _, best_at_each_step = gradient_dot_embedding_matrix.max(2)
     return best_at_each_step[0].detach().cpu().numpy()
+
+def get_best_candidates(model, batch, trigger_token_ids, cand_trigger_token_ids, beam_size=1):
+    """"
+    Given the list of candidate trigger token ids (of number of trigger words by number of candidates
+    per word), it finds the best new candidate trigger.
+    This performs beam search in a left to right fashion.
+    """
+    # first round, no beams, just get the loss for each of the candidates in index 0.
+    # (indices 1-end are just the old trigger)
+    loss_per_candidate = get_loss_per_candidate(0, model, batch, trigger_token_ids,
+                                                cand_trigger_token_ids)
+
+    # maximize the loss
+    top_candidates = heapq.nsmallest(beam_size, loss_per_candidate, key=itemgetter(1))
+
+    # top_candidates now contains beam_size trigger sequences, each with a different 0th token
+    for idx in range(1, len(trigger_token_ids)): # for all trigger tokens, skipping the 0th (we did it above)
+        loss_per_candidate = []
+        for cand, _ in top_candidates: # for all the beams, try all the candidates at idx
+            loss_per_candidate.extend(get_loss_per_candidate(idx, model, batch, cand,
+                                                             cand_trigger_token_ids))
+        top_candidates = heapq.nsmallest(beam_size, loss_per_candidate, key=itemgetter(1))
+    return max(top_candidates, key=itemgetter(1))[0]
+
+def get_loss_per_candidate(index, model, batch, trigger_token_ids, cand_trigger_token_ids):
+    """
+    For a particular index, the function tries all of the candidate tokens for that index.
+    The function returns a list containing the candidate triggers it tried, along with their loss.
+    """
+    if isinstance(cand_trigger_token_ids[0], (np.int64, int)):
+        print("Only 1 candidate for index detected, not searching")
+        return trigger_token_ids
+    # model.get_metrics(reset=True)
+    loss_per_candidate = []
+    # loss for the trigger without trying the candidates
+    curr_loss = get_model_output(model, batch, trigger_token_ids, device = device, output = 'loss').cpu().detach().numpy()
+    loss_per_candidate.append((deepcopy(trigger_token_ids), curr_loss))
+    
+    for cand_id in range(len(cand_trigger_token_ids[0])):
+        trigger_token_ids_one_replaced = deepcopy(trigger_token_ids) # copy trigger
+        trigger_token_ids_one_replaced[index] = cand_trigger_token_ids[index][cand_id] # replace one token
+        loss = get_model_output(model, batch, trigger_token_ids_one_replaced, device = device, output = 'loss').cpu().detach().numpy()
+        loss_per_candidate.append((deepcopy(trigger_token_ids_one_replaced), loss))
+    
+    return loss_per_candidate
